4d3f99ec995947baeefbb8a36d74770872725365
Ivan Klimov <klimov128@gmail.com>
2023-03-16 16:59:36 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import './css/styles.css';
+import './css/country-info.css';
+
 import debounce from 'lodash.debounce';
 const searchInput = document.querySelector('#search-box');
 const DEBOUNCE_DELAY = 300;
-const countryList = document.querySelector('.country-list');
-
+const countriesList = document.querySelector('.country-list');
+const countriyCard = document.querySelector('.country-info');
 searchInput.addEventListener(
   'input',
   debounce(searchCountries, DEBOUNCE_DELAY)
@@ -12,6 +14,8 @@ searchInput.addEventListener(
 function searchCountries(evt) {
   const currentInputValue = evt.target.value;
   if (currentInputValue === '') {
+    // countriesList.innerHTML = '';
+
     return;
   }
 
@@ -30,21 +34,69 @@ function fetchCountries(name) {
     })
     .then(countries => {
       if (countries.length > 10) {
+        countriesList.innerHTML = '';
         return console.log(
           'Too many matches found. Please enter a more specific name.'
         );
+      } else if (countries.length > 1) {
+        return createTmpltList(countries);
       }
-      console.log(countries);
-      return countries;
+      countriesList.innerHTML = '';
+
+      return createTmpltCard(countries);
+      // console.log(countries);
     })
     .catch(error => {
       console.log('Oops, there is no country with that name');
     });
 }
 
-// function createTmplt(countries) {
-//   countryList.insertAdjacentHTML('beforeend', countriesTemplate(countries));
-// }
+function createTmpltList(countries) {
+  countriesList.innerHTML = '';
+  countriesList.insertAdjacentHTML('beforeend', countriesTmpltList(countries));
+}
+
+function createTmpltCard(countries) {
+  countriyCard.innerHTML = '';
+  countriyCard.insertAdjacentHTML('beforeend', countriesTmpltCard(countries));
+}
+
+function countriesTmpltList(countries) {
+  return countries
+    .map(
+      country =>
+        `<li><img src="${country.flags.png}" alt="${country.name.official}" srcset="">
+  <p>${country.name.official}</p>
+</li>`
+    )
+    .join('');
+}
+
+function countriesTmpltCard(countries) {
+  return countries
+    .map(country => {
+      const languagesList = Object.values(country.languages);
+      const languagesListTmplt = languagesList
+        .map(item => `<li class="item-value">${item}</li>`)
+        .join('');
+      return `<h1 class="card-title"><img src="${country.flags.png}" alt="">
+      ${country.name.official}</h1>
+      <p class="item_title">capital: <span class="item-value">${country.capital}</span></p>
+      <p class="item_title">population: <span class="item-value">${country.population}</span></p>
+      <ul class="item_title">languages:${languagesListTmplt}</ul>
+`;
+    })
+    .join('');
+}
+
+{
+  /* <h1 class="card-title"><img src="${country.flags.png}" alt="">
+${country.name.official}</h1>
+<p class="item_title">capital: <span class="item-value">${country.capital}</span></p>
+<p class="item_title">population: <span class="item-value">${country.population}</span></p>
+<ul class="item_title">languages:${languagesListTmplt}</ul>   */
+}
+
 // name.official - повна назва країни
 // capital - столиця
 // population - населення
